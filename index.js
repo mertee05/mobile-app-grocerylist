a64b81c10a47734a7e1fc63e6e25c87bef0387ba
mertee05 <131835458+mertee05@users.noreply.github.com>
2023-07-20 12:50:25 -0700

    CSS elements

--- index.js
+++
@@ -1,6 +0,0 @@
-const inputFieldEl = document.getElementById("input-field");
-const addButtonEl = document.getElementById("add-button");
-addButtonEl.addEventListener("click", function () {
-  let inputField = inputFieldEl.value;
-  console.log(inputField);
-});
